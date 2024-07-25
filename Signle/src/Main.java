fc983f5421fd49841f21a4cbd8190bef684cb9eb
zlb <1101246047@qq.com>
2024-07-25 15:59:38 +0800

    修复main.java  master

--- Signle/src/Main.java
+++ Signle/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("11222");
         System.out.println("11222-master");
+        System.out.println("13333-master");
     }
 }
